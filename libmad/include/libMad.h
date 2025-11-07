03e409bf1d9b51ee8320e6fdb6b5e941ed53226f
Anton Pozharskiy <apozharski@gmail.com>
2025-11-07 14:43:33 +0100

    update headers

--- libmad/include/libMad.h
+++ libmad/include/libMad.h
@@ -7,6 +7,10 @@ extern "C" {
 
 
 #include <stdbool.h>
+#include <stdint.h>
+
+#define libmad_int int64_t
+#define libmad_real double
 
 typedef struct OptsDict OptsDict;
 typedef struct CNLPModel CNLPModel;
@@ -17,11 +21,11 @@ int libmad_create_options_dict(OptsDict** opts_ptr);
 
 int libmad_set_int_option(OptsDict* opts_ptr, char* name, int val);
 
-int libmad_set_long_option(OptsDict* opts_ptr, char* name, long val);
+int libmad_set_long_option(OptsDict* opts_ptr, char* name, libmad_int val);
 
 int libmad_set_float_option(OptsDict* opts_ptr, char* name, float val);
 
-int libmad_set_double_option(OptsDict* opts_ptr, char* name, double val);
+int libmad_set_double_option(OptsDict* opts_ptr, char* name, libmad_real val);
 
 int libmad_set_bool_option(OptsDict* opts_ptr, char* name, bool val);
 
@@ -31,8 +35,8 @@ int libmad_delete_options_dict(OptsDict* stats_ptr);
 
 int libmad_nlpmodel_create(CNLPModel** nlp_ptr_ptr,
 char* name,
-long nvar, long ncon,
-long nnzj, long nnzh,
+libmad_int nvar, libmad_int ncon,
+libmad_int nnzj, libmad_int nnzh,
 void* jac_struct, void* hess_struct,
 void* eval_f, void* eval_g,
 void* eval_grad_f, void* eval_jac_g,
@@ -40,34 +44,34 @@ void* eval_h,
 void* user_data);
 
 int libmad_nlpmodel_set_numerics(CNLPModel* nlp_ptr,
- double* x0, double* y0,
- double* lvar, double* uvar,
- double* lcon, double* ucon
+ libmad_real* x0, libmad_real* y0,
+ libmad_real* lvar, libmad_real* uvar,
+ libmad_real* lcon, libmad_real* ucon
 );
 
-int madnlp_get_obj(MadNLPExecutionStats* stats_ptr, double* out);
+int madnlp_get_obj(MadNLPExecutionStats* stats_ptr, libmad_real* out);
 
-int madnlp_get_solution(MadNLPExecutionStats* stats_ptr, double* out);
+int madnlp_get_solution(MadNLPExecutionStats* stats_ptr, libmad_real* out);
 
-int madnlp_get_constraints(MadNLPExecutionStats* stats_ptr, double* out);
+int madnlp_get_constraints(MadNLPExecutionStats* stats_ptr, libmad_real* out);
 
-int madnlp_get_multipliers(MadNLPExecutionStats* stats_ptr, double* out);
+int madnlp_get_multipliers(MadNLPExecutionStats* stats_ptr, libmad_real* out);
 
-int madnlp_get_multipliers_L(MadNLPExecutionStats* stats_ptr, double* out);
+int madnlp_get_multipliers_L(MadNLPExecutionStats* stats_ptr, libmad_real* out);
 
-int madnlp_get_multipliers_U(MadNLPExecutionStats* stats_ptr, double* out);
+int madnlp_get_multipliers_U(MadNLPExecutionStats* stats_ptr, libmad_real* out);
 
-int madnlp_get_bound_multipliers(MadNLPExecutionStats* stats_ptr, double* out);
+int madnlp_get_bound_multipliers(MadNLPExecutionStats* stats_ptr, libmad_real* out);
 
 int madnlp_get_success(MadNLPExecutionStats* stats_ptr, bool* out);
 
-int madnlp_get_iters(MadNLPExecutionStats* stats_ptr, long* out);
+int madnlp_get_iters(MadNLPExecutionStats* stats_ptr, libmad_int* out);
 
-int madnlp_get_primal_feas(MadNLPExecutionStats* stats_ptr, long* out);
+int madnlp_get_primal_feas(MadNLPExecutionStats* stats_ptr, libmad_int* out);
 
-int madnlp_get_dual_feas(MadNLPExecutionStats* stats_ptr, long* out);
+int madnlp_get_dual_feas(MadNLPExecutionStats* stats_ptr, libmad_int* out);
 
-int madnlp_get_status(MadNLPExecutionStats* stats_ptr, long* out);
+int madnlp_get_status(MadNLPExecutionStats* stats_ptr, libmad_int* out);
 
 int madnlp_delete_stats(MadNLPExecutionStats* stats_ptr);
 
