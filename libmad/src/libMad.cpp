09634da9d6a1ddd13e4912172716c1b039a1e621
Anton Pozharskiy <apozharski@gmail.com>
2025-11-07 14:56:17 +0100

    also bump libmad cpp

--- libmad/src/libMad.cpp
+++ libmad/src/libMad.cpp
@@ -2,24 +2,24 @@
 
 int libmad_create_options_dict(OptsDict** opts_ptr) { return 0; }
 
-int libmad_set_int_option(OptsDict* opts_ptr, char* name, int val) { return 0; }
+int libmad_set_int_option(OptsDict* opts_ptr, const char* name, int val) { return 0; }
 
-int libmad_set_long_option(OptsDict* opts_ptr, char* name, long val) { return 0; }
+int libmad_set_long_option(OptsDict* opts_ptr, const char* name, libmad_int val) { return 0; }
 
-int libmad_set_float_option(OptsDict* opts_ptr, char* name, float val) { return 0; }
+int libmad_set_float_option(OptsDict* opts_ptr, const char* name, float val) { return 0; }
 
-int libmad_set_double_option(OptsDict* opts_ptr, char* name, double val) { return 0; }
+int libmad_set_double_option(OptsDict* opts_ptr, const char* name, libmad_real val) { return 0; }
 
-int libmad_set_bool_option(OptsDict* opts_ptr, char* name, bool val) { return 0; }
+int libmad_set_bool_option(OptsDict* opts_ptr, const char* name, bool val) { return 0; }
 
-int libmad_set_string_option(OptsDict* opts_ptr, char* name, char* val) { return 0; }
+int libmad_set_string_option(OptsDict* opts_ptr, const char* name, char* val) { return 0; }
 
 int libmad_delete_options_dict(OptsDict* stats_ptr) { return 0; }
 
 int libmad_nlpmodel_create(CNLPModel** nlp_ptr_ptr,
-char* name,
-long nvar, long ncon,
-long nnzj, long nnzh,
+const char* name,
+libmad_int nvar, libmad_int ncon,
+libmad_int nnzj, libmad_int nnzh,
 void* jac_struct, void* hess_struct,
 void* eval_f, void* eval_g,
 void* eval_grad_f, void* eval_jac_g,
@@ -27,34 +27,34 @@ void* eval_h,
 void* user_data) { return 0; }
 
 int libmad_nlpmodel_set_numerics(CNLPModel* nlp_ptr,
- double* x0, double* y0,
- double* lvar, double* uvar,
- double* lcon, double* ucon
+ libmad_real* x0, libmad_real* y0,
+ libmad_real* lvar, libmad_real* uvar,
+ libmad_real* lcon, libmad_real* ucon
 ) { return 0; }
 
-int madnlp_get_obj(MadNLPExecutionStats* stats_ptr, double* out) { return 0; }
+int madnlp_get_obj(MadNLPExecutionStats* stats_ptr, libmad_real* out) { return 0; }
 
-int madnlp_get_solution(MadNLPExecutionStats* stats_ptr, double* out) { return 0; }
+int madnlp_get_solution(MadNLPExecutionStats* stats_ptr, libmad_real* out) { return 0; }
 
-int madnlp_get_constraints(MadNLPExecutionStats* stats_ptr, double* out) { return 0; }
+int madnlp_get_constraints(MadNLPExecutionStats* stats_ptr, libmad_real* out) { return 0; }
 
-int madnlp_get_multipliers(MadNLPExecutionStats* stats_ptr, double* out) { return 0; }
+int madnlp_get_multipliers(MadNLPExecutionStats* stats_ptr, libmad_real* out) { return 0; }
 
-int madnlp_get_multipliers_L(MadNLPExecutionStats* stats_ptr, double* out) { return 0; }
+int madnlp_get_multipliers_L(MadNLPExecutionStats* stats_ptr, libmad_real* out) { return 0; }
 
-int madnlp_get_multipliers_U(MadNLPExecutionStats* stats_ptr, double* out) { return 0; }
+int madnlp_get_multipliers_U(MadNLPExecutionStats* stats_ptr, libmad_real* out) { return 0; }
 
-int madnlp_get_bound_multipliers(MadNLPExecutionStats* stats_ptr, double* out) { return 0; }
+int madnlp_get_bound_multipliers(MadNLPExecutionStats* stats_ptr, libmad_real* out) { return 0; }
 
 int madnlp_get_success(MadNLPExecutionStats* stats_ptr, bool* out) { return 0; }
 
-int madnlp_get_iters(MadNLPExecutionStats* stats_ptr, long* out) { return 0; }
+int madnlp_get_iters(MadNLPExecutionStats* stats_ptr, libmad_int* out) { return 0; }
 
-int madnlp_get_primal_feas(MadNLPExecutionStats* stats_ptr, long* out) { return 0; }
+int madnlp_get_primal_feas(MadNLPExecutionStats* stats_ptr, libmad_real* out) { return 0; }
 
-int madnlp_get_dual_feas(MadNLPExecutionStats* stats_ptr, long* out) { return 0; }
+int madnlp_get_dual_feas(MadNLPExecutionStats* stats_ptr, libmad_real* out) { return 0; }
 
-int madnlp_get_status(MadNLPExecutionStats* stats_ptr, long* out) { return 0; }
+int madnlp_get_status(MadNLPExecutionStats* stats_ptr, libmad_int* out) { return 0; }
 
 int madnlp_delete_stats(MadNLPExecutionStats* stats_ptr) { return 0; }
 
